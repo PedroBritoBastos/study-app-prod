6bc2aa590ff6e14e0b1faf45775b101bf8a5deaf
pedro <pedrobritobastos234@gmail.com>
2026-03-14 14:25:39 -0300

    tela de loading

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,36 +6,35 @@ import { styles } from "@/styles/login/loginPage.styles";
 // components
 import { Input } from "@/src/components/ui/form/Input";
 import { Stack, Heading, Span, Button, Flex } from "@chakra-ui/react";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 export default function LoginPage() {
    return (
-      <Stack {...styles.container}>
+      <Suspense fallback={<Loading />}>
+         <Stack {...styles.container}>
 
-         {/* titulo da pagina de login */}
-         <Heading {...styles.heading}><Span {...styles.span}>Study</Span> App</Heading>
-
-         <form>
-
-            {/* legenda de login */}
-            <Heading {...styles.loginHeading}>Log In</Heading>
-
-            <Input
-               type="text"
-               name="username"
-               placeholder="Username"
-            />
-            <Input
-               type="password"
-               name="password"
-               placeholder="Password"
-            />
-
-            <Flex {...styles.submitButtonContainer}>
-               <Button {...styles.submitButton}>Log in</Button>
-               <Button {...styles.registerButton}>Cadastrar</Button>
-            </Flex>
-
-         </form>
-      </Stack>
+            {/* titulo da pagina de login */}
+            <Heading {...styles.heading}><Span {...styles.span}>Study</Span> App</Heading>
+            <form>
+               {/* legenda de login */}
+               <Heading {...styles.loginHeading}>Log In</Heading>
+               <Input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+               />
+               <Input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+               />
+               <Flex {...styles.submitButtonContainer}>
+                  <Button {...styles.submitButton}>Log in</Button>
+                  <Button {...styles.registerButton}>Cadastrar</Button>
+               </Flex>
+            </form>
+         </Stack>
+      </Suspense>
    );
 }
