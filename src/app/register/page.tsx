f93c69ef5908bb20bbfc9c1f4059a030a0d1299c
pedro <pedrobritobastos234@gmail.com>
2026-03-14 11:07:28 -0300

    register implementado

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -24,7 +24,7 @@ export default function RegisterPage() {
 
       try {
          await register(username, password);
-         router.push("/");
+         router.push("/login");
          router.refresh();
       } catch (error) {
          if (error instanceof Error) {
