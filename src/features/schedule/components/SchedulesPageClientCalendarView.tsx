38169566b661eeb51538fb48e200bd0229519eff
pedro <pedrobritobastos234@gmail.com>
2026-03-29 07:15:02 -0300

    dayOfWeek estilizado

--- src/features/schedule/components/SchedulesPageClientCalendarView.tsx
+++ src/features/schedule/components/SchedulesPageClientCalendarView.tsx
@@ -1,19 +1,16 @@
 "use client";
 
+import { styles } from "@/features/schedule/styles/schedulesPageClientCalendarView.styles";
+
 import { Grid, Text } from "@chakra-ui/react";
 
 const weekDays = ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"];
 
 export function SchedulesPageClientCalendarView() {
    return (
-      <Grid templateColumns="repeat(7, 1fr)" gap={2}>
+      <Grid {...styles.dayOfWeekGrid}>
          {weekDays.map((day) => (
-            <Text
-               key={day}
-               textAlign="center"
-               fontWeight="bold"
-               fontSize="sm"
-            >
+            <Text key={day} {...styles.dayOfWeek}>
                {day}
             </Text>
          ))}
---
+++ src/features/schedule/styles/schedulesPageClientCalendarView.styles.ts
@@ -0,0 +1,16 @@
+export const styles = {
+  dayOfWeekGrid: {
+    mt: 3,
+    templateColumns: "repeat(7, 1fr)",
+    gap: 2,
+  },
+  dayOfWeek: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "sm",
+    color: "white",
+    bg: "purple.600",
+    py: 1,
+    roundedTop: "md",
+  },
+};
