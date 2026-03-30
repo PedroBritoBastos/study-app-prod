3ad99eedb1312e0feac510917da2bb357d7e8cb3
pedro <pedrobritobastos234@gmail.com>
2026-03-30 12:05:42 -0300

    feat: apresenta o modo calendário

--- src/features/schedule/components/SchedulesPageClientCalendarDay.tsx
+++ src/features/schedule/components/SchedulesPageClientCalendarDay.tsx
@@ -2,6 +2,7 @@
 
 import { styles } from "@/features/schedule/styles/schedulesPageClientCalendarDay.styles";
 import { SchedulesDataType } from "@/features/schedule/types/GlobalScheduleData";
+import { useRouter } from "next/navigation";
 
 import { Card, Center, Stack, Text } from "@chakra-ui/react";
 
@@ -11,12 +12,22 @@ type SchedulesPageCalendarDayProps = {
 }
 
 export function SchedulesPageCalendarDay({ day, schedule }: SchedulesPageCalendarDayProps) {
+   const router = useRouter();
    const tasks = schedule?.currentScheduleTasks ?? [];
    const visibleTasks = tasks.slice(0, 3);
    const remaining = tasks.length - 3;
 
+   function handleCardClick(e: React.MouseEvent<HTMLElement>) {
+      e.stopPropagation();
+      if (schedule) router.replace(`/schedules/${schedule?.schedule.id}`);
+   }
+
    return (
-      <Card.Root {...styles.cardRoot}>
+      <Card.Root {...styles.cardRoot}
+         cursor={schedule ? "pointer" : "initial"}
+         _hover={schedule ? { bg: "gray.100" } : {}}
+         onClick={handleCardClick}
+      >
          <Card.Header {...styles.cardHeader}>
             <Center {...styles.dayContainer}>
                {day}
